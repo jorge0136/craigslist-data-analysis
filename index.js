e90215ae7e90eff2e1e8be9a55222f187912428c
Thomas George <Jorge0136@gmail.com>
2019-10-13 16:08:51 -0700

    Adds a van price search for seattle

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 let craigslist = require('node-craigslist');
-let client = new craigslist.Client({ city : 'seattle' });
+let util = require('util');
+
+let client = new craigslist.Client({
+  city: 'seattle'
+});
+
+let options = {
+  category: 'cta',
+  maxPrice: '90000',
+  minPrice: '100'
+};
+
+function remove_first_character(element) {
+  return element.substr(1, element.length)
+}
+
+let searchTerm = 'van';
 
 client
-  .list()
+  .search(options, searchTerm)
   .then((listings) => {
-    // play with listings here...
-    console.log(listings);
-    // listings.forEach((listing) => console.log(listing));
+
+    console.log(`There are ${listings.length} listings for ${searchTerm}`);
+
+    let allVanPrices = [];
+    // Prices come in as strings like '$8675309'. Here we remove the $ and cast to an integer.
+    listings.forEach((listing) => {
+      let cleanedVanPrice = parseInt(remove_first_character(listing.price));
+      allVanPrices.push(cleanedVanPrice);
+    });
+
+    console.log(allVanPrices);
   })
   .catch((err) => {
     console.error(err);
